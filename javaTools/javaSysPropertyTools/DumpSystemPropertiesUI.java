639d0276542d5971e539c4dff6475a71dcf39699
guo <swnykimo@gmail.com>
2015-09-24 17:57:40 +0800

    modify DumpSystemPropertiesUI.java's architecture

--- javaTools/javaSysPropertyTools/DumpSystemPropertiesUI.java
+++ javaTools/javaSysPropertyTools/DumpSystemPropertiesUI.java
@@ -4,20 +4,11 @@ import java.util.Properties;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-public class DumpSystemPropertiesUI {
-    public static void main(String[] args) {
-        JFrame win = new JFrame("a VM's System Properties");
-        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        win.setContentPane(new SystemPropertiesPanel());
-        win.pack();
-        win.setVisible(true);
-    }//end main
-}//end DumpSystemPropertiesUI
+public class DumpSystemPropertiesUI extends JPanel {
 
-class SystemPropertiesPanel extends JPanel {
     JTextArea propertiesTA = new JTextArea(30, 100);
 
-    public SystemPropertiesPanel() {
+    public DumpSystemPropertiesUI() {
         this.setLayout(new BorderLayout());
         this.add(new JScrollPane(propertiesTA), BorderLayout.CENTER);
 
@@ -27,10 +18,17 @@ class SystemPropertiesPanel extends JPanel {
 		Iterator<String> it = pKeys.iterator();
 
         while(it.hasNext()) {
-            //String key = (String)it.next();
             String key = it.next();
             propertiesTA.append("" + key + "=" + p.get(key) + "\n");
         }//end while
     }// end constructor
-}//end SystemPropertiesPanel
+
+    public static void main(String[] args) {
+        JFrame win = new JFrame("a VM's System Properties");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setContentPane(new DumpSystemPropertiesUI());
+        win.pack();
+        win.setVisible(true);
+    }//end main
+}//end DumpSystemPropertiesUI
 
